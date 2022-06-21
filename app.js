89869aaacee6964b25439ed6815e543cc0bd7b93
Isaac Fuenmayor <isaacfuenmayora@gmail.com>
2022-06-21 01:06:45 -0400

    Add makeGrid function, fix container class style

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const gridContainer = document.getElementsByClassName('container')
+const gridContainer = document.querySelector('.container')
 const gridSize = 16
 
+makeGrid()
+
+function makeGrid() {
+    const squareWidth = 100.0/gridSize+'%';
+    for(let i=0; i<gridSize; ++i){
+        for(let j=0; j<gridSize; ++j){
+            const gridSquare = document.createElement('div')
+            gridSquare.classList.add('grid-square')
+            gridSquare.style.width = squareWidth
+            gridSquare.style.paddingTop = squareWidth
+            gridContainer.appendChild(gridSquare)
+        }
+    }
+
+}
